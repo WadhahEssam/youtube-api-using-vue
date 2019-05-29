b9e3a872513eb7380c55f0333081cfa6ca4471be
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-05-30 00:27:44 +0300

    making the actual damn search

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-    <h1>Hello There !</h1>
+  <div>
+    <h1>Search In Youtube</h1>
+    <SearchBar @termChangedEvent="termChanged"></SearchBar>
+  </div>
 </template>
 
 <script>
+import SearchBar from './components/SearchBar';
+import searchYoutube from 'youtube-api-v3-search';
+const API = 'AIzaSyBKEiICVjCPdwpnaJhvdTf1zWvJc4suiQQ';
+
 export default {
   name: 'App',
+  // do not forget to register the compnent
+  // it is not like react where all you have
+  // to do is to import the component  
+  components: {
+    SearchBar
+  },
+  methods: {
+    async termChanged(term) {
+      let videos = await searchYoutube(API,{
+        q: term,
+        part: 'snippet',
+        type:'video',
+      });
+      console.log(videos);
+    }
+  }
 }
 </script>
 
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input @input="searchInputChanged" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+    methods: {
+      searchInputChanged (event) {
+        this.$emit('termChangedEvent', event.target.value);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
