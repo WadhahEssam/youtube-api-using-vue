aab297f89fab5b7286d1600bc10c215456989f75
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-05-29 22:06:31 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+// this file has to be named exactly like this
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
-
+// this is another way to tell vue to show somehting inside of 
+// the #app div and turn it to html
+// the h is a simpol for ( createElement ) fucntion 
+//
+// they made it as short as possiable with the arrow function
+// 
+// the .$mount fucnction is the same thing as specifing el: '#app'
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render:  h => h(App),
+});
+
+
+
